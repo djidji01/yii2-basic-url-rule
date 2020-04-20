cb138c278ccf680759cbc08c23b3bead35705db1
djidji01 <djidji01@gmail.com>
2020-04-20 17:44:18 +0200

    init

---
+++ README.md
@@ -0,0 +1,84 @@
+UrlRule for default basic  logic in parsing and creating pretty Url.
+======================================================================
+Parsing and creating url with arguments pretified as well instead of in query string format. Route and argument segments of url are separated by the UrlManager->routeParam
+
+Conventions
+----------
+Argument is prettified only if its  name is declared as  action method parameter otherwise it stay in query string format;
+Arguments are orderer by the way corresponding action method parameters are declared;
+Optional Argument nth binds to its corresponding action method parameter only if all arguments --nth are explicitly passed. --nth argments are inserted at their respective places in created url if were nor explicit.
+Arguments less than required or more than action method parameters show 404.
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist djidji/yii2-default-url-rule "*"
+```
+
+or add
+
+```
+"djidji/yii2-default-url-rule": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by declaring in rules of Urlmanager :
+
+```
+'rules' => [
+    // ...other url rules...
+    [
+        'class' => 'djidji\DefaultUrlRule',
+        // complete file path to save routes.
+        'routesFile' => '@app/config/routes.php'
+    ],
+]
+
+```
+Examples
+--------
+
+```
+class PostController extends Controller
+{
+    public function actionIndex($category,$year=2015,$tag='')
+    {
+
+    }
+    public function actionView($id)
+    {
+
+    }
+}
+```
+To create Url:
+
+```Url::to(['post/index', 'year' => 2014, 'category' => 'php'])``` creates ```/index.php/post/index/r/php/2014``` ;
+```Url::to(['post/index', 'category' => 'php'])``` creates ```/index.php/post/index/r/php```
+```Url::to(['post/index', 'category' => 'php','tag'=>'programming'])``` creates ```/index.php/post/index/r/php/2015/programming```. default value of parameter 'year' is inserted at its place
+
+```Url::to(['post/index','year' => 2014, 'category' => 'php'])``` result to `false` because the argument for required `$category` parameter is not passed;
+```Url::to(['post/view', 'id' => 100])``` creates ```/index.php/post/view/r/100``` ;
+```Url::to(['post/view', 'id' => 100, 'source' => 'ad'])``` creates ```creates /index.php/post/view/r/100?source=ad```. Because "source" argument is not declared as actionView methoth, it is appended as a query parameter in the created URL.
+
+To parse Url
+
+```/index.php/post/index```  result to `false` because actionIndex has required firs parameter that need argments to be passed.
+
+```/index.php/post/index/r/php``` is parsed to ```['post/index', ['category' => 'php']]```;
+```/index.php/post/index/r/php/2015/programming``` is parsed to ```['post/index', ['category' => 'php','tag'=>'programming','year'=>2015]]```;
+```/index.php/post/index/r/php/programming``` is parsed to ```['post/index', ['category' => 'php','year'=>'programming']]```;
+```/index.php/post/view/r/100?source=ad``` is parsed to ['post/view', ['id' => 100]]
+```/index.php/post/view/r/100/ad?source=ad``` result to `false` because actionView method expect one argment instead of two.
+
---
+++ src/DefaultUrlRule.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace djidji;
+
+use Yii;
+use yii\base\BaseObject;
+use yii\web\UrlRuleInterface;
+use yii\caching\CacheInterface;
+
+class DefaultUrlRule extends BaseObject  implements UrlRuleInterface
+{
+    public $routesFile='@app/config/routes.php';
+
+    /**
+     * Initializes this rule by ensuring the path for routes file exists.
+     */
+    public function init()
+    {
+        $this->routesFile = Yii::getAlias($this->routesFile);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUrl($manager, $route, $urlArgs)
+    {
+        if ($manager->enableStrictParsing) {
+            return false;
+        }
+        $args='';
+        $cacheKey =$route.'&'.implode('$',array_keys($urlArgs));
+
+        if (\file_exists($this->routesFile)) {
+            $routes=require $this->routesFile;
+        }
+        if (!isset($routes[$cacheKey])) {
+
+            if ($parts=Yii::$app->createController($route)) {
+                $params=$this->validateActionId($parts[0],$parts[1],$urlArgs);
+                if (is_array($params)) {
+                    $routeConf=$routes[$cacheKey]=['route'=>$route,'params'=>array_keys($params)];
+
+                    file_put_contents($this->routesFile, "<?php  \nreturn ".var_export($routes,true).";");
+
+                }else {
+                    return false;
+                }
+            }
+        }else{
+            $routeConf=$routes[$cacheKey];
+        }
+
+        foreach ($routeConf['params'] as $argName) {
+            if (array_key_exists($argName, $urlArgs)) {
+                $args .= '/'.$urlArgs[$argName];
+                unset($urlArgs[$argName]);
+            }
+        }
+
+
+        $route.=$args!==''?"/{$manager->routeParam}{$args}":'';
+        if ($manager->suffix !== null) {
+            $route .= $manager->suffix;
+        }
+        if (!empty($urlArgs) && ($urlArgs = http_build_query($urlArgs)) !== '') {
+            $route .= '?' . $urlArgs;
+        }
+
+        return $route;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parseRequest($manager, $request)
+    {
+        if ($manager->enableStrictParsing) {
+            return false;
+        }
+        $suffix = (string) $manager->suffix;
+        $pathInfo = $request->getPathInfo();
+        $normalized = false;
+        if ($manager->normalizer !== false) {
+            $pathInfo = $manager->normalizer->normalizePathInfo($pathInfo, $suffix, $normalized);
+        }
+        if ($suffix !== '' && $pathInfo !== '') {
+            $n = strlen($manager->suffix);
+            if (substr_compare($pathInfo, $manager->suffix, -$n, $n) === 0) {
+                $pathInfo = substr($pathInfo, 0, -$n);
+                if ($pathInfo === '') {
+                    // suffix alone is not allowed
+                    return false;
+                }
+            } else {
+                // suffix doesn't match
+                return false;
+            }
+        }
+
+        $result=[$pathInfo, []];
+        $pathInfo=trim($pathInfo,'/');
+        if (strpos($pathInfo, "/{$manager->routeParam}/") !== false) {
+            list($route,$urlArgs)=explode("/{$manager->routeParam}/",$pathInfo);
+            $urlArgs=explode('/',$urlArgs);
+            $cacheKey =$route.'&'.count($urlArgs);
+
+            if (\file_exists($this->routesFile)) {
+                $routes=require $this->routesFile;
+            }
+
+            if (!isset($routes[$cacheKey])) {
+                if ($parts=Yii::$app->createController($route)) {
+                    if ($params=$this->validateActionId($parts[0],$parts[1],$urlArgs,true)) {
+                        $routeKey=$route.'&'.implode('$',array_keys($params));
+                        if (!isset($routes[$routeKey])) {
+                            $routes[$routeKey]=['route'=>$route,'params'=>array_keys($params)];
+                        }
+                        $routes[$cacheKey]=$routeKey;
+                        file_put_contents($this->routesFile, "<?php  \nreturn ".var_export($routes,true).";");
+
+                    }
+                }
+            }else{
+                $params=array_combine($routes[$routes[$cacheKey]]['params'], $urlArgs);
+
+            }
+            $result=[$route,$params];
+        }
+
+        if ($normalized) {
+            // pathInfo was changed by normalizer - we need also normalize route
+            return $manager->normalizer->normalizeRoute($result);
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * Verifie if the actionId and args have a corresponding Controller method and parameters
+     *
+     * @param string $actionId actionId to be validates
+     * @param array $args passed arguments
+     * @param bool $maxArgs verify that arguments should not be more than delared method parameters
+     * @return array|bool index being method parameter name and value being passed argument or false if method not found
+     */
+    public function validateActionId($controller,$actionId,$args,$maxArgs=false)
+    {
+        $actionId=$actionId?:$controller->defaultAction;
+        $actionMap= $controller->actions();
+        if (isset($actionMap[$actionId])) {
+            $actionClass=isset($actionMap[$actionId]['class'])?$actionMap[$actionId]['class']:$actionMap[$actionId];
+            $method = new \ReflectionMethod($actionClass, 'run');
+        }
+        if (preg_match('/^(?:[a-z0-9_]+-)*[a-z0-9_]+$/', $actionId)) {
+            $methodName = 'action' . str_replace(' ', '', ucwords(str_replace('-', ' ', $actionId)));
+            if (method_exists($controller, $methodName)) {
+                $method = new \ReflectionMethod($controller, $methodName);
+            }
+        }
+
+        if (isset($method) && $method->isPublic()) {
+            if ($method->getNumberOfRequiredParameters()<=count($args)) {
+                if ($maxArgs && count($args)>$method->getNumberOfParameters()) {
+                    return false;
+                }
+                return $this->routifyUrlParams($method,$args);
+
+            }
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Map and sort url or route Arguments in the way defined by the controller method
+     *
+     * This is called by [[validateActionId()]] when method corresponding to actionId is found
+     * @param \ReflectionMethod $method \ReflectionMethod instance of action method
+     * @param array $urlArgs arguments to be passed
+     * @return array index being method parameter name and value being passed argument
+     */
+    public function routifyUrlParams($method,$urlArgs)
+    {
+        $actionParams = [];
+        $optionals=[];
+        foreach ($method->getParameters()as $index => $param) {
+            $name = $param->getName();
+            if (isset($urlArgs[$index])) {
+                $urlArgs[$name]=$urlArgs[$index];
+                unset($urlArgs[$index]);
+            }
+            if (array_key_exists($name, $urlArgs)) {
+
+                if ($param->isDefaultValueAvailable()) {
+                    $actionParams+=$optionals;
+                    $optionals=[];
+
+                }
+                $actionParams[$name] = $urlArgs[$name];
+                unset($urlArgs[$name]);
+            }elseif ($param->isDefaultValueAvailable()) {
+                $optionals[$name]=$param->getDefaultValue();
+
+            }
+        }
+
+        return $actionParams;
+
+    }
+
+}
